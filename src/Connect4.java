04bf96d4e8156006ed614232d57be48f6a19e73b
Gian Esteves <31361395+itsuni@users.noreply.github.com>
2019-06-10 23:50:19 -0400

    Update Connect4.java
    all win conditions finished
    
    need to fix reading in moves

--- src/Connect4.java
+++ src/Connect4.java
@@ -2,6 +2,12 @@
 
 import java.util.Scanner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Connect4 {
 
     public static int rows = 6;
@@ -9,14 +15,32 @@ public class Connect4 {
 
     public static int turn = 1;
 
+    public static int auto;
+    public static int autoFile;
+
     public static int[][] board = new int[rows][columns];
 
     public static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Welcome to connect 4");
         System.out.println("");
+
+        System.out.println("Play a game with human or test inputs? 0 or 1");
+        auto = input.nextInt();
+
+        //Select test file
+        if (auto == 1) {
+            System.out.println("Which test file?  1, 2, 3, or mystery (4)");
+
+            autoFile = input.nextInt();
+
+            if (autoFile > 5 || autoFile < 0) {
+                System.out.println("Try again...");
+                autoFile = input.nextInt();
+            }
+        }
 
         //Print empty board
         printBoard();
@@ -32,6 +56,7 @@ public class Connect4 {
             System.out.println("Turn #" + turn);
             printBoard();
 
+            //End conditions
             if (checkWin(1) == true || checkWin(2) == true) {
                 System.out.println("Game over!");
 
@@ -41,6 +66,12 @@ public class Connect4 {
                     System.out.println("1 Wins!");
                 }
 
+                break;
+            }
+
+            //TIE
+            if (turn > 42) {
+                System.out.println("Tied Game");
                 break;
             }
         }
@@ -57,7 +88,8 @@ public class Connect4 {
 
     }
 
-    public static void setPiece(int color) {
+    public static void setPiece(int color) throws IOException {
+
         System.out.println("Please choose a column, 1 - 7");
 
         int choice = input.nextInt();
@@ -99,8 +131,15 @@ public class Connect4 {
         }
     }
 
+    public static int autoPiece(){
+        
+        int result = 0;
+
+        return result;
+    }
+    
     public static boolean checkWin(int player) {
-        if (checkHori(player) == true || checkVert(player) == true) {
+        if (checkHori(player) == true || checkVert(player) == true || checkDiagUp(player) == true || checkDiagDown(player) == true) {
             return true;
         }
         return false;
@@ -121,6 +160,28 @@ public class Connect4 {
         for (int i = 0; i < rows - 3; i++) {
             for (int j = 0; j < columns; j++) {
                 if (board[i][j] == player && board[i + 1][j] == player && board[i + 2][j] == player && board[i + 3][j] == player) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkDiagUp(int player) {
+        for (int i = 3; i < rows; i++) {
+            for (int j = 0; j < columns - 3; j++) {
+                if (board[i][j] == player && board[i - 1][j + 1] == player && board[i - 2][j + 2] == player && board[i - 3][j + 3] == player) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkDiagDown(int player) {
+        for (int i = 3; i < rows; i++) {
+            for (int j = 3; j < columns; j++) {
+                if (board[i][j] == player && board[i - 1][j - 1] == player && board[i - 2][j - 2] == player && board[i - 3][j - 3] == player) {
                     return true;
                 }
             }
